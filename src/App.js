00c32f5695a34f5337a01527d6fb1795a6672d3d
Zackery Mowrer <Zackmowrer@gmail.com>
2019-05-18 13:36:45 -0400

    Add initial state to App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,30 @@ import './App.css';
 import Header from "./components/Header";
 
 class App extends React.Component {
+    constructor(props) {
+        // Pass props to base class
+        super(props);
+
+        // Initialize state
+        this.state = {
+            // Photos for default tags
+            defaultPhotos: {
+                cats: [],
+                dogs: [],
+                computers: [],
+            },
+
+            // Search results
+            searchResults: [],
+
+            // Any error that may occur
+            error: null,
+
+            // Whether or not we are currently loading something
+            isLoading: true,
+        };
+    }
+
     render() {
         return (
             <div className="App container">
