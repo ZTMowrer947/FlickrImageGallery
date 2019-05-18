31c105966e5d2993815cff64db843035e1b09965
Zackery Mowrer <Zackmowrer@gmail.com>
2019-05-18 14:18:53 +0000

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+// Imports
+import React from "react";
+
+// Component
+const Header = () => (
+    <header className="App-header">
+        <h1>Flickr Image Gallery</h1>
+    </header>
+);
+
+// Export
+export default Header;
