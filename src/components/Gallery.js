c278018a9d0e285d3ac245ac6420fd088557ebb3
Zackery Mowrer <Zackmowrer@gmail.com>
2019-05-18 16:38:59 -0400

    Add no result case to Gallery component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -25,11 +25,18 @@ const Gallery = ({ photos, isLoading }) => {
         );
     })
 
+    const noResultsMessage = (
+        <li className="not-found">
+            <h3>No Results Found</h3>
+            <p>You search did not return any results. Please try again.</p>
+        </li>
+    )
+
     return (
         <div className="photo-container">
             <h2>Results</h2>
             <ul>
-                {galleryPhotos}
+                {galleryPhotos.length > 0 ? galleryPhotos: noResultsMessage}
             </ul>
         </div>
     );
