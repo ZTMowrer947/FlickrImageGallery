f036d0d3bd11de468267cb0e56a3aa103324d2c3
Zackery Mowrer <Zackmowrer@gmail.com>
2019-05-18 13:24:44 -0400

    Add title prop to GalleryPhoto component

--- src/components/GalleryPhoto.js
+++ src/components/GalleryPhoto.js
@@ -3,13 +3,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // Component
-const GalleryPhoto = ({ photoUrl }) => (
-    <img src={photoUrl} />
+const GalleryPhoto = ({ photoUrl, title }) => (
+    <img src={photoUrl} alt={title} />
 )
 
 // Prop Types
 GalleryPhoto.propTypes = {
-    photoUrl: PropTypes.string.isRequired
+    photoUrl: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
 };
 
 // Export
