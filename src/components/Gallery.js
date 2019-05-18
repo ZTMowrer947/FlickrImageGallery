d11231ab8e10c8934c98f3bbb2c417c2557a20bb
Zackery Mowrer <Zackmowrer@gmail.com>
2019-05-18 13:27:51 -0400

    Add Gallery component

---
+++ src/components/Gallery.js
@@ -0,0 +1,40 @@
+// Imports
+import React from "react";
+import PropTypes from "prop-types";
+import GalleryPhoto from "./GalleryPhoto";
+
+// Component
+const Gallery = ({ photos }) => {
+    const galleryPhotos = photos.map(({ farmId, id, secret, server, title}) => {
+        const url = `https://farm${farmId}.staticflickr.com/${server}/${id}_${secret}.jpg`;
+
+        return (
+            <li key={id}>
+                <GalleryPhoto photoUrl={url} title={title} />
+            </li>
+        );
+    })
+
+    return (
+        <div className="photo-container">
+            <h2>Results</h2>
+            <ul>
+                {galleryPhotos}
+            </ul>
+        </div>
+    );
+};
+
+// Prop Types
+Gallery.propTypes = {
+    photos: PropTypes.arrayOf(PropTypes.shape({
+        farmId: PropTypes.number.isRequired,
+        id: PropTypes.string.isRequired,
+        secret: PropTypes.string.isRequired,
+        server: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+    })),
+}
+
+// Export
+export default Gallery;
