77079178a723b60476e05d1ac1ff018bf9ced0ac
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-23 13:49:59 -0500

    Add Redux store configuration function

---
+++ src/store/index.ts
@@ -0,0 +1,40 @@
+// Imports
+import { RouterAction, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import {
+    Store,
+    Middleware,
+    applyMiddleware,
+    compose,
+    createStore,
+    StoreEnhancer,
+} from "redux";
+import AppState from "./AppState";
+import { GalleryAction } from "./gallery/types";
+import createRootReducer from "./reducer";
+
+// History setup
+export const history = createBrowserHistory();
+
+// Store factory
+const configureStore = (): Store<AppState, RouterAction | GalleryAction> => {
+    // Define middleware list and enhancer
+    const middleware: Middleware[] = [routerMiddleware(history)];
+    const middlewareEnhancer = applyMiddleware(...middleware);
+
+    // Define enhancer list and composed enhancer
+    const enhancers: StoreEnhancer[] = [middlewareEnhancer];
+    const enhancer: StoreEnhancer = compose(...enhancers);
+
+    // Create root reducer
+    const reducer = createRootReducer(history);
+
+    // Create Redux store
+    const store = createStore(reducer, enhancer);
+
+    // Return created store
+    return store;
+};
+
+// Export
+export default configureStore;
