71ec311b157b3d66c1171c1478fbd5c1ae2b7ef7
ZTMowrer947 <Zackmowrer@gmail.com>
2019-11-23 10:59:36 -0500

    Add bootstrap styling to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 // Imports
 import React from "react";
+import Container from "react-bootstrap/Container";
 
 // Component
-const App: React.FC = () => <h1>Hello, World!</h1>;
+const App: React.FC = () => (
+    <Container fluid>
+        <h1>Hello, World!</h1>
+    </Container>
+);
 
 // Export
 export default App;
