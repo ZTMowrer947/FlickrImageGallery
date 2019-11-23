cf0d84a04a95ee37dbd2061d62d92ba48406fa43
ZTMowrer947 <Zackmowrer@gmail.com>
2019-11-23 09:52:27 -0500

    Adjust config file indentation

--- .babelrc.js
+++ .babelrc.js
@@ -1,43 +1,43 @@
 module.exports = api => {
-  // Determine if we are in a development/testing environment
-  const isDev = api.env(["development", "dev", "test", "testing", "staging"]);
+    // Determine if we are in a development/testing environment
+    const isDev = api.env(["development", "dev", "test", "testing", "staging"]);
 
-  // Define presets
-  const presets = [
-    // Environment
-    [
-      "@babel/preset-env",
-      {
-        // Use builtins based on usage
-        useBuiltIns: "usage",
+    // Define presets
+    const presets = [
+        // Environment
+        [
+            "@babel/preset-env",
+            {
+                // Use builtins based on usage
+                useBuiltIns: "usage",
 
-        // Use core-js 3
-        corejs: 3
-      }
-    ],
+                // Use core-js 3
+                corejs: 3,
+            },
+        ],
 
-    ["@babel/preset-react", { development: isDev }],
+        ["@babel/preset-react", { development: isDev }],
 
-    // TypeScript
-    "@babel/preset-typescript"
-  ];
+        // TypeScript
+        "@babel/preset-typescript",
+    ];
 
-  // Define plugins
-  const plugins = [
-    // Class properties
-    "@babel/plugin-proposal-class-properties",
+    // Define plugins
+    const plugins = [
+        // Class properties
+        "@babel/plugin-proposal-class-properties",
 
-    // TypeScript 3.7 features
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator"
-  ];
+        // TypeScript 3.7 features
+        "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-proposal-nullish-coalescing-operator",
+    ];
 
-  // If not in production, transform babel runtime
-  if (isDev) plugins.push("@babel/plugin-transform-runtime");
+    // If not in production, transform babel runtime
+    if (isDev) plugins.push("@babel/plugin-transform-runtime");
 
-  // Return configuration object
-  return {
-    presets,
-    plugins
-  };
+    // Return configuration object
+    return {
+        presets,
+        plugins,
+    };
 };
