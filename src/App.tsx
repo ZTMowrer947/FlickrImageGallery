89fde560495e45c7248e68aebeb959c867d15e06
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-23 18:25:55 -0500

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { hot } from "react-hot-loader/root";
+import Header from "./components/Header";
 
 // Component
 const App: React.FC = () => (
     <Container fluid>
+        <Header />
         <h1>Hello, World!</h1>
     </Container>
 );
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+// Imports
+import React from "react";
+
+// Component
+const Header: React.FC = () => (
+    <header className="w-100">
+        <h1 className="text-center">Flickr Image Gallery</h1>
+    </header>
+);
+
+// Export
+export default Header;
