a8bf1dbf14aeb35e89d568eaf1b7477ea97aaae7
ZTMowrer947 <Zackmowrer@gmail.com>
2019-11-23 11:51:19 -0500

    Add photo action creators

---
+++ src/store/creators.ts
@@ -0,0 +1,34 @@
+// Imports
+import { ActionCreator } from "redux";
+import {
+    FetchPhotosStartAction,
+    FETCH_PHOTOS_START,
+    FetchPhotosSuccessAction,
+    FetchPhotosFailedAction,
+    Photo,
+    FETCH_PHOTOS_DONE,
+} from "./types";
+
+// Action creators
+export const startPhotoFetch: ActionCreator<FetchPhotosStartAction> = (
+    tag: string
+) => ({
+    type: FETCH_PHOTOS_START,
+    payload: { tag },
+});
+
+export const finishPhotoFetch: ActionCreator<
+    FetchPhotosSuccessAction | FetchPhotosFailedAction
+> = (photos: Photo[], error?: Error) => {
+    if (error)
+        return {
+            type: FETCH_PHOTOS_DONE,
+            error: true,
+            payload: error,
+        };
+
+    return {
+        type: FETCH_PHOTOS_DONE,
+        payload: photos,
+    };
+};
