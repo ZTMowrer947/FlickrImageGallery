ca81167b3214069503d89823cf1c89b7aefd336e
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-23 13:38:56 -0500

    Add gallery reducer function

---
+++ src/store/gallery/index.ts
@@ -0,0 +1,3 @@
+// Exports
+export { default } from "./reducer";
+export { startPhotoFetch, finishPhotoFetch } from "./creators";
---
+++ src/store/gallery/reducer.ts
@@ -0,0 +1,62 @@
+// Imports
+import { Reducer } from "redux";
+import { tassign } from "tassign";
+import {
+    GalleryState,
+    GalleryAction,
+    FETCH_PHOTOS_START,
+    FETCH_PHOTOS_DONE,
+    Photo,
+} from "./types";
+
+// Initial gallery state
+const initialState: GalleryState = {
+    error: undefined,
+    loading: false,
+    photoIds: [],
+    photosById: {},
+};
+
+// Reducer function
+const gallery: Reducer<GalleryState, GalleryAction> = (
+    state = initialState,
+    action
+) => {
+    switch (action.type) {
+        case FETCH_PHOTOS_START:
+            return tassign<GalleryState, Partial<GalleryState>>(state, {
+                loading: true,
+                error: undefined,
+            });
+
+        case FETCH_PHOTOS_DONE:
+            if (action.error) {
+                const error = action.payload as Error;
+
+                return tassign<GalleryState, Partial<GalleryState>>(state, {
+                    loading: false,
+                    error,
+                });
+            }
+
+            const photos = action.payload as Photo[];
+
+            const photoIds = photos.map(photo => photo.id);
+            const photosById = photos.reduce(
+                (acc, photo) => tassign(acc, { [photo.id]: photo }),
+                {} as { [id: string]: Photo }
+            );
+
+            return tassign(state, {
+                loading: false,
+                photoIds,
+                photosById,
+            });
+
+        default:
+            return state;
+    }
+};
+
+// Export
+export default gallery;
