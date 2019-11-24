785e482ea9d0b1229964274fec5c7b61500bcea7
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-24 07:27:22 -0500

    Add photo fetching service

---
+++ src/services/fetchPhotos.ts
@@ -0,0 +1,22 @@
+// Imports
+import axios from "axios";
+import { from, Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Photo } from "../store/gallery/types";
+
+// Response type
+interface FetchPhotosResponse {
+    photos: Photo[];
+}
+
+// Photo fetching service
+const fetchPhotos = (tag: string): Observable<Photo[]> => {
+    return from(
+        axios.get<FetchPhotosResponse>(
+            `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_KEY}&tags=${tag}&per_page=24&format=json&nojsoncallback=1`
+        )
+    ).pipe(map(response => response.data.photos));
+};
+
+// Export
+export default fetchPhotos;
