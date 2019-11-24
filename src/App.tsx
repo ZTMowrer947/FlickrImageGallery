1967c0fd9fd2f62d8b13e76c77784fce4ce5f853
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-24 16:02:01 -0500

    Add basic routing to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { hot } from "react-hot-loader/root";
+import { Switch, Redirect, Route } from "react-router-dom";
 import Header from "./components/Header";
+import ConnectedGallery from "./containers/ConnectedGallery";
 
 // Component
 const App: React.FC = () => (
     <Container fluid>
         <Header />
         <h1>Hello, World!</h1>
+        <Switch>
+            <Redirect from="/" to="/photos/dogs" exact />
+            <Route path="/photos/:tag" component={ConnectedGallery} />
+        </Switch>
     </Container>
 );
 
