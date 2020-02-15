64cf44d9ea318858d390fbc9cf2f40f91e80b309
Zackery Mowrer <Zackmowrer@gmail.com>
2020-02-15 13:40:59 -0500

    Add actions for redux store

---
+++ src/store/actions.ts
@@ -0,0 +1,33 @@
+// Imports
+import { createAction } from '@reduxjs/toolkit';
+
+import Photo from '../models/Photo';
+import Tag from '../models/Tag';
+
+// Actions
+export const startPhotoFetch = createAction<string>('PHOTO_FETCH_STARTED');
+export const photoFetchSuccess = createAction(
+    'PHOTO_FETCH_SUCCESS',
+    (tagId: string, photos: Photo[]) => {
+        // Create tag
+        const tag: Tag = {
+            id: tagId,
+            photoIds: photos.map(photo => photo.id),
+        };
+
+        // Return payload with tag and photo data
+        return {
+            payload: {
+                tag,
+                photos,
+            },
+        };
+    }
+);
+export const photoFetchFailed = createAction(
+    'PHOTO_FETCH_FAILED',
+    (error: Error) => ({
+        payload: error,
+        error: true,
+    })
+);
