9a8bbdf2c431a5c380bd34f01b70f754353c72e0
Zackery Mowrer <Zackmowrer@gmail.com>
2020-02-15 18:08:17 -0500

    Add redux batch enhancer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,13 @@
 // Imports
 import { configureStore } from '@reduxjs/toolkit';
+import { reduxBatch } from '@manaflair/redux-batch';
+
 import rootReducer from './reducers';
 
 // Store setup
 const store = configureStore({
     reducer: rootReducer,
+    enhancers: baseEnhancers => [reduxBatch, ...baseEnhancers, reduxBatch],
 });
 
 // Exports
