a95c111a4b89335c75609e702b0a98be3e488492
Zackery Mowrer <Zackmowrer@gmail.com>
2020-02-15 14:08:34 -0500

    Add base reducer

---
+++ src/store/reducers/base.ts
@@ -0,0 +1,50 @@
+// Imports
+import { createReducer } from '@reduxjs/toolkit';
+
+import {
+    startPhotoFetch,
+    photoFetchSuccess,
+    photoFetchFailed,
+} from '../actions';
+
+// Initial state
+const initialState = {
+    isFetching: false,
+    error: null as Error | null,
+};
+
+// Reducer
+const base = createReducer(initialState, {
+    // Photo fetch has begun
+    [startPhotoFetch.type]: state => {
+        // Set fetching state
+        state.isFetching = true;
+
+        // Clear any previous errors
+        state.error = null;
+    },
+
+    // Photo fetch completed successfully
+    [photoFetchSuccess.type]: state => {
+        // Unset fetching state
+        state.isFetching = false;
+    },
+
+    // Photo fetch has failed
+    [photoFetchFailed.type]: (
+        state,
+        action: ReturnType<typeof photoFetchFailed>
+    ) => {
+        // Unset fetching state
+        state.isFetching = false;
+
+        // Attach error
+        state.error = action.payload;
+    },
+});
+
+// State type
+export type BaseState = Readonly<ReturnType<typeof base>>;
+
+// Export
+export default base;
