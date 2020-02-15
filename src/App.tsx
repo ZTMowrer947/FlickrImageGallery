d26cdef49ea14dca72e073bd5f21d902d6e1ebe4
Zackery Mowrer <Zackmowrer@gmail.com>
2020-02-15 08:13:09 -0500

    Add fluid container to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 // Imports
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 import './App.css';
 
 // App component
 const App: React.FC = () => {
-    return <div></div>;
+    return (
+        <Container fluid>
+            <h1>Hello!</h1>
+        </Container>
+    );
 };
 
 // Export
