b96a95989ef6c0b549e49259a844f6a69d07c74c
Zackery Mowrer <Zackmowrer@gmail.com>
2020-02-16 12:55:43 -0500

    Add route for GalleryPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 // Imports
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Header from './components/Header';
+import GalleryPage from './pages/GalleryPage';
 
 import './App.css';
 
 // App component
 const App: React.FC = () => {
     return (
-        <Container fluid className="h-100">
+        <Container
+            fluid
+            className="h-100 d-flex flex-column align-items-center"
+        >
             <Header />
+            <Switch>
+                <Redirect from="/" to="/tagged/dogs" exact />
+                <Route
+                    path="/tagged/:tag"
+                    render={routeProps => <GalleryPage {...routeProps} />}
+                />
+            </Switch>
         </Container>
     );
 };
--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -8,17 +8,17 @@ const MainNav: React.FC = () => (
     <nav>
         <Nav justify variant="pills" as="ul">
             <Nav.Item as="li">
-                <LinkContainer to="/photos/dogs" exact>
+                <LinkContainer to="/tagged/dogs" exact>
                     <Nav.Link>Dogs</Nav.Link>
                 </LinkContainer>
             </Nav.Item>
             <Nav.Item as="li">
-                <LinkContainer to="/photos/cats" exact>
+                <LinkContainer to="/tagged/cats" exact>
                     <Nav.Link>Cats</Nav.Link>
                 </LinkContainer>
             </Nav.Item>
             <Nav.Item as="li">
-                <LinkContainer to="/photos/computers" exact>
+                <LinkContainer to="/tagged/computers" exact>
                     <Nav.Link>Computers</Nav.Link>
                 </LinkContainer>
             </Nav.Item>
