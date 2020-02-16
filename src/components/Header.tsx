3a7a229b6cbc292c43c1139ee683ffdba1edc798
Zackery Mowrer <Zackmowrer@gmail.com>
2020-02-16 13:31:24 -0500

    Add SearchForm component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 // Imports
 import React from 'react';
 import MainNav from './MainNav';
+import SearchForm from './SearchForm';
 
 // Component
 const Header: React.FC = () => {
@@ -8,6 +9,7 @@ const Header: React.FC = () => {
         <header className="w-50 mx-auto d-flex flex-column align-items-center">
             <h1 className="text-center">Flickr Image Gallery</h1>
 
+            <SearchForm />
             <MainNav />
         </header>
     );
